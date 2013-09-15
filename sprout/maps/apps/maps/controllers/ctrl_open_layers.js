51298b6739eff4f49f029fcb84f8f729f6eb6a5e
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2013-09-15 15:08:37 +0200

    less verbose logging

--- sprout/maps/apps/maps/controllers/ctrl_open_layers.js
+++ sprout/maps/apps/maps/controllers/ctrl_open_layers.js
@@ -346,13 +346,13 @@ Maps.openLayersController = SC.ArrayController.create(
             // Actual re-ordering
             var oldIndex = record.get('order');  // -1 to convert from ranking # to index
             //@if(debug)
-            console.log("reordering item[" + oldIndex + "]=" + record + " to " + proposedInsertionIndex);
+            console.log("reordering item[" + oldIndex + "]=" + record.get('name') + " to " + proposedInsertionIndex);
             //@endif
             if (proposedInsertionIndex < oldIndex) {
                 // Move up list
                 for (var i = proposedInsertionIndex; i < oldIndex; i++) {
                     //@if(debug)
-                    console.log("Moving item[" + i + "]=" + this.objectAt(i) + " up to " + (i + 1));
+                    console.log("Moving item[" + i + "]=" + this.objectAt(i).get('name') + " up to " + (i + 1));
                     //@endif
                     this.objectAt(i).set('order', i + 1);  // add 1 to convert from ranking to sequence #
                 }
@@ -360,7 +360,7 @@ Maps.openLayersController = SC.ArrayController.create(
                 // Move down list
                 for (var i = oldIndex; i < proposedInsertionIndex; i++) {
                     //@if(debug)
-                    console.log("Moving item[" + i + "]=" + this.objectAt(i) + " down to " + (i - 1));
+                    console.log("Moving item[" + i + "]=" + this.objectAt(i).get('name') + " down to " + (i - 1));
                     //@endif
                     this.objectAt(i).set('order', i - 1);  // add 1 to convert from ranking to sequence #
                 }
