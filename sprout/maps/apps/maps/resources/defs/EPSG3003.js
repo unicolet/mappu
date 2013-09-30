28eb1e192d5d8184688b27a36f179a118347d229
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2013-09-30 14:11:46 +0200

    remove a line commented out a long time ago

--- sprout/maps/apps/maps/resources/defs/EPSG3003.js
+++ sprout/maps/apps/maps/resources/defs/EPSG3003.js
@@ -1,4 +1,2 @@
-
-//Proj4js.defs["EPSG:3003"] = "+proj=tmerc +lat_0=0 +lon_0=9 +k=0.999600 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs +towgs84=-104.1,-49.1,-9.9,0.971,-2.917,0.714,-11.68 +to +proj=latlong +datum=WGS84";
 Proj4js.defs["EPSG:3003"] = "+proj=tmerc +lat_0=0 +lon_0=9 +k=0.999600 +x_0=1500000 +y_0=0 +ellps=intl +units=m +no_defs +towgs84=-104.1,-49.1,-9.9,0.971,-2.917,0.714,-11.68";
 
